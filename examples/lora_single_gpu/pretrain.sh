a5b178960a86ff54b03f9f3044f6a5c976a5a5b1
josutk <josue.nascimento@jusbrasil.com.br>
2024-04-27 12:05:48 -0300

    feat: add model

--- examples/lora_single_gpu/pretrain.sh
+++ examples/lora_single_gpu/pretrain.sh
@@ -3,28 +3,31 @@
 CUDA_VISIBLE_DEVICES=0 python ../../src/train_bash.py \
     --stage pt \
     --do_train \
-    --model_name_or_path josu/gemma-pt-br4 \
-    --dataset wiki_pt \
-    --dataset_dir /drive/MyDrive/pt_data/data/pt_wiki_corpus.txt \
-    --finetuning_type lora \
-    --lora_target q_proj,v_proj \
-    --output_dir ametista \
+    --model_name_or_path josu/gemma-pt-br-galore-layer \
+    --dataset c4 \
+    --dataset_dir ../../data \
+    --finetuning_type full \
+    --output_dir gemma_pedregulho1 \
     --overwrite_cache \
     --overwrite_output_dir \
     --cutoff_len 1024 \
-    --preprocessing_num_workers 16 \
-    --per_device_train_batch_size 1 \
+    --preprocessing_num_workers 1 \
+    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --warmup_steps 20 \
-    --save_steps 100 \
-    --eval_steps 100 \
+    --save_steps 1000 \
+    --eval_steps 50000 \
     --evaluation_strategy steps \
-    --load_best_model_at_end \
-    --learning_rate 5e-5 \
-    --num_train_epochs 1.5 \
+    --learning_rate 3e-5 \
+    --num_train_epochs 3.0 \
     --val_size 0.1 \
     --plot_loss \
-    --fp16
+    --flash_attn True \
+    --use_galore True \
+    --galore_rank 256 \
+    --galore_update_interval 200   \
+    --galore_target mlp,self_attn \
+    --galore_layerwise \
+    --pure_bf16
